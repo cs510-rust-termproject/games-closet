38194e902ceef16424b5c8ac555231b4e8865314
LBIV4 <lanebartoniv@gmail.com>
2019-07-10 16:50:27 -0700

    Creating project structure

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # games-closet
+
+A Rust crate for CS510 - Rust Programming aiming to implement various basic board/card games.
+
+##Contributors
+Andre Mukhsia
+Lane Barton
---
+++ src/main.rs
@@ -0,0 +1,8 @@
+// Copyright © 2019 Andre Mukhsia, Lane Barton
+// [This program is licensed under the "MIT License"]
+// Please see the file LICENSE in the source
+// distribution of this software for license terms.
+
+fn main() {
+    panic!("Nothing is implemented yet!")
+}
