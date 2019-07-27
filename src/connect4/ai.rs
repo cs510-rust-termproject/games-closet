4138bf5f9ce9aa2bbae635487afa64b5e60bb347
LBIV4 <lanebartoniv@gmail.com>
2019-07-27 12:46:19 -0700

    Fixing code with introduction of core.rs

--- src/connect4/ai.rs
+++ src/connect4/ai.rs
@@ -3,29 +3,27 @@
 // Please see the file LICENSE in the source
 // distribution of this software for license terms.
 
-mod core::*;
-
-const BOARD_SIZE: (i32, i32) = (6, 7);
+use connect4::core;
 
 struct GameState {
-    mut spaces: [[0i32;7];6];
+    spaces: [[i32;7];6]
 }
 
 impl GameState {
     fn get_board(&self) -> [[i32;7];6] {
         self.spaces
-    
+    }
 
     fn get_column_height(&self, col: u32) -> i32 {
-        let i = 0;
-        while i < 7 && self.spaces[col][i] != 0 {
-            i++
+        let mut i = 0;
+        while i < 7 && self.spaces[col as usize][i] != 0 {
+            i += 1;
         }
-        i
+        i as i32
     }
 
     fn on_board(&self, x: i32, y: i32) -> bool {
-        x >= 0 && x < BOARD_SIZE.0 && y >= 0 && y < BOARD_SIZE.1
+        x >= 0 && x < core::BOARD_SIZE.0 && y >= 0 && y < core::BOARD_SIZE.1
     }
 
     //Method to get a "max" run including a starting point in a target direction for a given team.
@@ -35,30 +33,30 @@ impl GameState {
     //(as the space presumably prevents an actual run of 4). Cases with a run of 4 prior to space will return 4, except for edge 
     //case where run goes from start and then completely in reverse direction. This can be caught by calling this method with reverse 
     //direction
-    fn get_run_in_direction(&self, start: (i32, i32), direction: (i32, i32), team: u32) -> u32 {
+    fn get_run_in_direction(&self, start: (i32, i32), dir: (i32, i32), team: i32) -> i32 {
         let mut dir_active = true;
         let mut rev_active = true;
         let mut space_used = false;
-        let mut run_len = 1u32; //Start with dropped token
+        let mut run_len = 1i32; //Start with dropped token
         let mut i = 1; //Start one beyond dropped token
-        while run_len <= 4 && (dir1_active || dir2_active) {
-            dir_active = dir1_active && self.on_board(start.0+i*dir.0, start.1+i*dir.1);
-            rev_active = dir2_active && self.on_board(start.0-i*dir.0, start.1-i*dir.1);
+        while run_len <= 4 && (dir_active || rev_active) {
+            dir_active = dir_active && self.on_board(start.0+i*dir.0, start.1+i*dir.1);
+            rev_active = rev_active && self.on_board(start.0-i*dir.0, start.1-i*dir.1);
             //Do reverse case first for edge case of AASA_A is treated as a run of 4 and not 3 with a space
             if rev_active {
-                if self.board[start.0-i*dir.0][start.1-i*dir.1] == team {
+                if self.spaces[(start.0-i*dir.0) as usize][(start.1-i*dir.1) as usize] == team {
                     run_len += 1;
                 } else {
                     rev_active = false;
                 }
             }
             if dir_active {
-                let val = self.board[start.0+i*dir.0][start.1+i*dir.1];
+                let val = self.spaces[(start.0+i*dir.0) as usize][(start.1+i*dir.1) as usize];
                 if val == team {
                     run_len += 1;
                     //Check for contiguous run of 4 before space, return immediately to prevent odd cases with spaces
                     if !space_used && run_len >= 4 {
-                        return 4u32;
+                        return 4i32;
                     }
                 } else if val == 0 && !space_used {
                     space_used = true;
@@ -78,18 +76,18 @@ impl GameState {
     //Method to return an array of runs from a start location for a given team, where array[i] returns the number of runs
     //of length i-1. Accounts for all eight directions, but may have false duplicates (e.g. a run BAAAB will return have two
     //runs of length 3 for team A even though technically its the same run)
-    fn get_runs_from_point(&self, start: (i32, i32), team: u32) -> [u32;4] {
-        let mut output = [0u32;4];
+    fn get_runs_from_point(&self, start: (i32, i32), team: i32) -> [i32;4] {
+        let mut output = [0i32;4];
         let directions = vec![(1, 0), (1, 1), (0, 1), (-1, 1)];
         for dir in directions {
-            output[self.get_run_in_direction(start, dir, team)-1] += 1;
-            output[self.get_run_in_direction(start, (-1*dur.0, -1*dir.1), team)-1] += 1;
+            output[(self.get_run_in_direction(start, dir, team)-1) as usize] += 1;
+            output[(self.get_run_in_direction(start, (-1*dir.0, -1*dir.1), team)-1) as usize] += 1;
         }
         output
     }
 }
 
-struct MoveCheck {
+/*struct MoveCheck {
     team: u32,
     board: GameState,
     runs: [0u32;4]
@@ -98,8 +96,8 @@ struct MoveCheck {
 impl MoveCheck {
     fn init(&mut self, board: GameState, moveCol: u32, team: u32) -> MoveCheck {
         self.team = team;
-        self.board = board.clone();
-        self.board.add_disc(moveCol, team);
+        self.spaces = board.clone();
+        self.spaces.add_disc(moveCol, team);
     }
 
     fn compare(&self, other: MoveCheck) -> i32 {
@@ -116,14 +114,14 @@ impl MoveCheck {
     }
 
 
-}
+}*/
 
 fn pick_optimal_move(state: GameState) -> i32 {
-    let mut startBoard = GameState.get_board().clone();
-    
+    let mut startBoard = state.get_board().clone();
+    0
 }
 
-fn find_win_probability(board: GameState, team: u32, currMove: u32, lastMove: u32) -> f32 {
+/*fn find_win_probability(board: GameState, team: u32, currMove: u32, lastMove: u32) -> f32 {
     //Check win for AI turn
     for i in 0..7 {
         
@@ -132,4 +130,4 @@ fn find_win_probability(board: GameState, team: u32, currMove: u32, lastMove: u3
 
 fn main() {
     panic!("Connect 4 is not implemented yet!")
-}
+}*/
