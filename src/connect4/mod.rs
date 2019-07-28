50b4978d4c9cecdd28168595b0fdbf893cad04ec
LBIV4 <lanebartoniv@gmail.com>
2019-07-28 14:09:59 -0700

    Added mod file for connect4

---
+++ src/connect4/mod.rs
@@ -0,0 +1,7 @@
+// Copyright © 2019 Andre Mukhsia, Lane Barton
+// [This program is licensed under the "MIT License"]
+// Please see the file LICENSE in the source
+// distribution of this software for license terms.
+
+pub mod core;
+pub mod ai;
